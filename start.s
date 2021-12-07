4bc9d6d3e347c75e9cc59119b5779e1ba01b77b2
sharmavins23 <sharmavins23@gmail.com>
2021-12-07 00:11:36 -0500

    fixed formatting

--- start.s
+++ start.s
@@ -10,27 +10,26 @@
 .equ        COUNTER,    0xf0000
 
 _start:
-            ldr         r0,=BASE
-            ldr         r2,=COUNTER
-            ldr         r1,=SET_BIT3
+            ldr         r0, =BASE
+            ldr         r2, =COUNTER
+            ldr         r1, =SET_BIT3
             str         r1, [r0,#GPFSEL2]
 
             ldr         r1, =SET_BIT21
             str         r1, [r0,#GPSET0]
 
 looop:
-	        str         r1, [r0,#GPSET0]	    @ TURN ON
-	        mov         r10,#0              @ DELAY
+	        str         r1, [r0,#GPSET0]	@ TURN ON
+	        mov         r10, #0             @ DELAY
 delay:                                      @ Loop to large number
-		    add         r10,r10,#1
-		    cmp         r10,r2	
+		    add         r10, r10, #1
+		    cmp         r10, r2	
 		    bne         delay	
-	        str         r1,[r0,#GPCLR0]     @TURN OFF
-	        mov         r10,#0
-delay2:                                     @DELAY2
-		    add         r10,r10,#1
-		    cmp         r10,r2	
+	        str         r1, [r0,#GPCLR0]    @ TURN OFF
+	        mov         r10, #0
+delay2:                                     @ DELAY2
+		    add         r10, r10, #1
+		    cmp         r10, r2	
 		    bne         delay2
             
             b           looop
-            
